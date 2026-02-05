f5f7a2b81d4c05093132ccbdb520234d1aefa2f3
Mr-Greatlion <146362818+Mr-Greatlion@users.noreply.github.com>
2026-02-05 16:08:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,15 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,   # Only trusted origins
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
-    allow_methods=["*"],         # Includes OPTIONS automatically
+    allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 # -------------------------------------------------
